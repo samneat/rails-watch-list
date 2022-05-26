eb25825eef8686020a1767669c49ba660a3d3409
sam neat <samjneat@gmail.com>
2022-05-26 12:09:09 +0100

    improved style

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id, message: "movie in list" }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'Movie in list' }
 end
