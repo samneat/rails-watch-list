33a641e835dff6693baae68b8610ebf9263ad343
sam neat <samjneat@gmail.com>
2022-05-26 15:06:06 +0100

    bookmark features added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
